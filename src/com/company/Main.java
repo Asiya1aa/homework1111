bf0220dcd9b8afd24d85fdd71cd0e520076861e7
Asiya76 <asiyaasanalieva1234@gmail.com>
2021-11-10 14:37:53 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String wejieh;
+        final int NUM = 20;
+        String word = "vgfyu";
+        wejieh = NUM + word;
+        if (NUM < 0)
+            System.out.println("вы сохранили отрицательное число");
+        if (NUM > 0)
+            System.out.println("вы сохранили положительное число");
+        else;
+        System.out.println("вы сохранили нуль");
+
+    }
+}
